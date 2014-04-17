5c87bc424f3f999fc50ef19c4404ac32baf0c2fc
dag <dag.rende@find-out.se>
2014-04-17 07:15:30 +0200

    fixed width and height sizing but not on window resize

--- app/scripts/controllers/edit.js
+++ app/scripts/controllers/edit.js
@@ -2,6 +2,12 @@
 
 angular.module('scrappyApp')
   .controller('EditCtrl', function ($scope, $routeParams) {
+    var setFullHeight = function() {
+      $('#tinymce').height((window.innerHeight - 70) + 'px');
+    }
+    setFullHeight();
+//    $(window).bind("resize", setFullHeight);
+
     var scrapId = $routeParams.scrapId;
     $scope.scrapText = "scrap " + scrapId;
   });
