42d81027d96e8ab9804e4b67ba9c1e19018d4174
dag <dag.rende@find-out.se>
2014-04-19 22:07:59 +0200

    now editor is saved regularly (each 3s)

--- app/scripts/controllers/edit.js
+++ app/scripts/controllers/edit.js
@@ -4,21 +4,37 @@ angular.module('scrappyApp')
   .controller('EditCtrl', function ($scope, $routeParams, Scraps, fbURL, $firebase) {
     setFullHeight();
     $scope.scrap = $firebase(new Firebase(fbURL + $routeParams.scrapId));
-    $scope.$on("$destroy", function(){
-      $scope.scrap.$save();
-    });
+    saver($scope);
   })
   .controller('CreateCtrl', function($scope, Scraps, $firebase, fbURL) {
     setFullHeight();
     $scope.scrap = {text: ""};
     Scraps.$add($scope.scrap).then(function(newRef) {
       $scope.scrap = $firebase(new Firebase(fbURL + newRef.name()));
-      $scope.$on("$destroy", function(){
-        $scope.scrap.$save();
-      });
+      saver($scope);
     });
   });
 
 function setFullHeight() {
   $('#tinymce').height((window.innerHeight - 70) + 'px');
+}
+
+function saver($scope) {
+  var pendingSaves = 0;
+  var intervalID = window.setInterval(function () {
+    if (pendingSaves == 0) {
+      pendingSaves++;
+      $scope.scrap.$save().then(function () {
+        pendingSaves--;
+        console.log('save ok');
+      });
+    } else {
+      if (pendingSaves > 5) {
+        console.log('saving problem - pendingSaves=' + pendingSaves);
+      }
+    }
+  }, 3000);
+  $scope.$on("$destroy", function () {
+    window.clearInterval(intervalID);
+  });
 }
